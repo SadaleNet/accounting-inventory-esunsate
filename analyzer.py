9fa85518fe6f57698b6ad6e8da2b9a38de468677
SadaleNet <SadaleNet@users.noreply.github.com>
2025-12-24 15:31:49 +0800

    Added latest records for R&D cost and a purchase

--- analyzer.py
+++ analyzer.py
@@ -116,7 +116,7 @@ AVAILABLE_TYPES = {
 }
 
 # List of available projects
-AVAILABLE_ITEMS = ["ilonena"]
+AVAILABLE_ITEMS = ["ilonena", "ilomusiali"]
 
 # Pass 1 - parse fields into data table
 data_table = []
